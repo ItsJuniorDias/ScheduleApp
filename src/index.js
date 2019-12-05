284d5f0257713e58de7e72b257b2b5aa018c911b
itsjuniordias <juniordias_@live.com>
2019-12-05 09:41:37 -0200

    Add component Icon. MDB

--- src/index.js
+++ src/index.js
@@ -22,35 +22,80 @@ class App extends Component {
 				{
 					id: 2,
 					time: "10:30",
-					title: "Daily Standup Meeting",
-					location: "Warsaw Spire Office",
-					description: "Meeting World!"
+					title: "Daily Standup Meeting (recurring)",
+					location: "Warsaw Spire Office"
 				},
+				{ id: 3, time: "11:00", title: "Call with HRs" },
+				{
+					id: 4,
+					time: "11:00",
+					title: "Lunch with Timothy",
+					location: "Canteen",
+					description:
+						"Project evaluation ile declaring a variable and using an if statement is a fine way to conditionally render a component, sometimes you might want to use a"
+				}
 			]
 		};
 	}
 
+
 	render() {
 	return (
-		<React.Fragment>
-		<MDBContainer>
-		<MDBRow>
-      <MDBCol md="9">
-		  {this.state.events.map(event => (
-				<Event 
-		    key={event.id}
-        id={event.id}
-        time={event.time}
-        title={event.title}
-        location={event.location}
-        description={event.description}
-				/>
-       ))}
-      </MDBCol>
-      <MDBCol md="3" />
-     </MDBRow>
-		</MDBContainer>
-		</React.Fragment>
+	<React.Fragment> 
+		<MDBContainer> 
+			<MDBRow> 
+				<MDBCol  md="9" className="mb-r">  
+					<h2 className="text-uppercase my-3"> Today: </h2>
+					<div id="schedule-items"> 
+						{this.state.events.map(event => (
+							<Event
+							key={event.id}
+							id={event.id}
+							time={event.time}
+							title={event.title}
+							location={event.location}
+							description={event.description}
+							/> 
+						))}
+					</div>
+					<MDBRow className="mb-4"> 
+					<MDBCol xl="3" md="6" className="mx-auto text-center">
+              <MDBBtn color="info" rounded>
+               Add Event
+						  </MDBBtn>
+						 </MDBCol>
+					</MDBRow>
+				</MDBCol>
+        <MDBCol md="3">
+          <h3 className="text-uppercase my-3"> Schedule </h3>
+					<h6 className="my-3">
+						Its going to be busy that today.You have{""}
+						<b>{this.state.events.length} events </b>  
+             Today.
+					</h6>
+					<h1 className="my-3"> 
+            <MDBRow> 
+							<MDBCol xs="3" className="text-center">
+									<MDBIcon icon="sun" fixed/>
+							</MDBCol>
+							<MDBCol xs="9"> Sunny </MDBCol>
+						</MDBRow>
+						<MDBRow>
+							<MDBCol xs="3" className="text-center">
+								<MDBIcon icon="thermometer-three-quarters" fixed/>
+							</MDBCol>
+							 <MDBCol xs="9"> 23°C </MDBCol>
+						</MDBRow>
+						</h1>
+              <p>
+                Don't forget your sunglasses. Today will dry and sunny, becoming
+                warm in the afternoon with temperatures of between 20 and 25
+                degrees.
+              </p>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </React.Fragment>
 	);
 	}
 }
