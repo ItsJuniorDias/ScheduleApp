5c4f924b2f18178054e61a8b9d6a589befa3c28c
itsjuniordias <juniordias_@live.com>
2019-12-05 09:46:31 -0200

    update files

--- src/index.js
+++ src/index.js
@@ -40,53 +40,52 @@ class App extends Component {
 
 
 	render() {
-	return (
-	<React.Fragment> 
-		<MDBContainer> 
-			<MDBRow> 
-				<MDBCol  md="9" className="mb-r">  
-					<h2 className="text-uppercase my-3"> Today: </h2>
-					<div id="schedule-items"> 
-						{this.state.events.map(event => (
-							<Event
-							key={event.id}
-							id={event.id}
-							time={event.time}
-							title={event.title}
-							location={event.location}
-							description={event.description}
-							/> 
-						))}
-					</div>
-					<MDBRow className="mb-4"> 
-					<MDBCol xl="3" md="6" className="mx-auto text-center">
-              <MDBBtn color="info" rounded>
-               Add Event
-						  </MDBBtn>
-						 </MDBCol>
-					</MDBRow>
-				</MDBCol>
-        <MDBCol md="3">
-          <h3 className="text-uppercase my-3"> Schedule </h3>
-					<h6 className="my-3">
-						Its going to be busy that today.You have{""}
-						<b>{this.state.events.length} events </b>  
-             Today.
-					</h6>
-					<h1 className="my-3"> 
-            <MDBRow> 
-							<MDBCol xs="3" className="text-center">
-									<MDBIcon icon="sun" fixed/>
-							</MDBCol>
-							<MDBCol xs="9"> Sunny </MDBCol>
-						</MDBRow>
-						<MDBRow>
-							<MDBCol xs="3" className="text-center">
-								<MDBIcon icon="thermometer-three-quarters" fixed/>
-							</MDBCol>
-							 <MDBCol xs="9"> 23°C </MDBCol>
-						</MDBRow>
-						</h1>
+    return (
+      <React.Fragment>
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol md="9" className="mb-r">
+              <h2 className="text-uppercase my-3">Today:</h2>
+              <div id="schedule-items">
+                {this.state.events.map(event => (
+                  <Event
+                    key={event.id}
+                    id={event.id}
+                    time={event.time}
+                    title={event.title}
+                    location={event.location}
+                    description={event.description}
+                  />
+                ))}
+              </div>
+              <MDBRow className="mb-4">
+                <MDBCol xl="3" md="6" className="mx-auto text-center">
+                  <MDBBtn color="info" rounded>
+                    Add Event
+                  </MDBBtn>
+                </MDBCol>
+              </MDBRow>
+            </MDBCol>
+            <MDBCol md="3">
+              <h3 className="text-uppercase my-3">Schedule</h3>
+              <h6 className="my-3">
+                It's going to be busy that today. You have{" "}
+                <b>{this.state.events.length} events </b> today.
+              </h6>
+              <h1 className="my-3">
+                <MDBRow>
+                    <MDBCol xs="3" className="text-center">
+                      <MDBIcon icon="sun" fixed />
+                    </MDBCol>
+                    <MDBCol xs="9">Sunny</MDBCol>
+                  </MDBRow>
+                  <MDBRow>
+                    <MDBCol xs="3" className="text-center">
+                    <MDBIcon icon="thermometer-three-quarters" fixed />
+                    </MDBCol>
+                    <MDBCol xs="9">23°C</MDBCol>
+                  </MDBRow>
+              </h1>
               <p>
                 Don't forget your sunglasses. Today will dry and sunny, becoming
                 warm in the afternoon with temperatures of between 20 and 25
@@ -96,8 +95,8 @@ class App extends Component {
           </MDBRow>
         </MDBContainer>
       </React.Fragment>
-	);
-	}
+    );
+  }
 }
 class Event extends Component {
 
